ab3d401523445eb6d0b832cb8305a4d1cc6a5a01
Nhi Ly <44133684+nnhilys@users.noreply.github.com>
2025-11-07 00:58:34 +0900

    remove default double click

--- src/editor/provider.tsx
+++ src/editor/provider.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react'
+import type { StateNode, TLClickEventInfo } from 'tldraw'
 import { Tldraw } from 'tldraw'
 import { usePages } from '../page/context'
 import { renderPages } from '../page/render'
@@ -7,6 +8,10 @@ import { EDITOR_CAMERA } from './camera'
 import { editorComponents } from './component/components'
 import { editorShapeUtils } from './shape-util/main'
 import { editorTools } from './tool/main'
+
+type IdleStateNode = StateNode & {
+  handleDoubleClickOnCanvas: (info: TLClickEventInfo) => void
+}
 
 export function EditorProvider(props: { children: ReactElement }): ReactElement {
   const { children } = props
@@ -23,6 +28,16 @@ export function EditorProvider(props: { children: ReactElement }): ReactElement 
       onMount={(editor) => {
         editor.user.updateUserPreferences({ colorScheme: theme })
         renderPages(editor, pages)
+
+        // Prevent create text on double click
+        // Reference: https://tldraw.dev/examples/custom-double-click-behavior
+        const selectIdleState = editor.getStateDescendant<IdleStateNode>('select.idle')
+        if (!selectIdleState)
+          throw new Error('SelectTool Idle state not found')
+
+        const removeDoubleClickOnCanvasHandler = () => void {}
+        selectIdleState.handleDoubleClickOnCanvas
+          = removeDoubleClickOnCanvasHandler.bind(selectIdleState)
       }}
     >
       {children}
